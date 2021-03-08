55cfe33cd6babd72e273f0bfb8106088e1a53795
Ayon95 <mushfiqurrahman78@yahoo.com>
2021-03-07 20:46:08 -0500

    Added a list component and clearing functionality

--- src/List.js
+++ src/List.js
@@ -1,11 +1,21 @@
 import React from 'react';
 
-const List = () => {
-  return (
-    <>
-      <h2>list component</h2>
-    </>
-  );
+const List = ({ people }) => {
+	return (
+		<>
+			{people.map((person) => {
+				return (
+					<article key={person.id} className="person">
+						<img src={person.image} alt={person.name} />
+						<div>
+							<h4>{person.name}</h4>
+							<p>{person.age} years </p>
+						</div>
+					</article>
+				);
+			})}
+		</>
+	);
 };
 
 export default List;
