dd9f85497b4ba70466b31880f904d413e799d399
Ayon95 <mushfiqurrahman78@yahoo.com>
2021-03-13 20:54:12 -0500

    Added styling to PageNotFound

--- src/PageNotFound.js
+++ src/PageNotFound.js
@@ -7,7 +7,7 @@ function PageNotFound() {
 			<SadFace className="icon-sad-face" />
 			<h1 className="page-not-found__heading-1">404 Error</h1>
 			<h2 className="page-not-found__heading-2">Sorry, we could not find the page you were looking for.</h2>
-			<Link to="/" className="go-home">
+			<Link to="/" className="btn go-home">
 				Go back to home page
 			</Link>
 		</div>
