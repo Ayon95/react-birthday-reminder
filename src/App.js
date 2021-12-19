581b41616e553223003e2236bd60e195bd72d146
Ayon95 <mushfiqurrahman78@yahoo.com>
2021-12-19 18:48:18 -0500

    implemented code splitting in App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
+import React, { Suspense } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navigation/Navbar.js';
-import Home from './components/Home.js';
-import AllBirthdays from './components/Birthdays/AllBirthdays.js';
-import UpcomingBirthdays from './components/Birthdays/UpcomingBirthdays.js';
-import Search from './components/Search.js';
-import PageNotFound from './components/PageNotFound.js';
 import GlobalContextProvider from './contexts/GlobalContext.js';
 import AuthContextProvider from './contexts/AuthContext.js';
-import Signup from './components/Forms/Signup.js';
-import Login from './components/Forms/Login.js';
 import ProtectedRoute from './components/ProtectedRoute.js';
-import ResetPassword from './components/Forms/ResetPassword.js';
-import EditForm from './components/Forms/EditForm.js';
-import AddForm from './components/Forms/AddForm.js';
+import LoadingSpinner from './components/LoadingSpinner.js';
+// Dynamically importing components when they are required by the user (for code-splitting)
+const Signup = React.lazy(() => import('./components/Forms/Signup.js'));
+const Login = React.lazy(() => import('./components/Forms/Login.js'));
+const ResetPassword = React.lazy(() => import('./components/Forms/ResetPassword.js'));
+const Home = React.lazy(() => import('./components/Home.js'));
+const AllBirthdays = React.lazy(() => import('./components/Birthdays/AllBirthdays.js'));
+const UpcomingBirthdays = React.lazy(() => import('./components/Birthdays/UpcomingBirthdays.js'));
+const AddForm = React.lazy(() => import('./components/Forms/AddForm.js'));
+const EditForm = React.lazy(() => import('./components/Forms/EditForm.js'));
+const Search = React.lazy(() => import('./components/Search.js'));
+const PageNotFound = React.lazy(() => import('./components/PageNotFound.js'));
 
 function App() {
 	return (
@@ -23,45 +26,33 @@ function App() {
 						<Navbar />
 
 						<main>
-							<Switch>
-								<Route path="/signup">
-									<Signup />
-								</Route>
-
-								<Route path="/login">
-									<Login />
-								</Route>
-
-								<Route path="/reset-password">
-									<ResetPassword />
-								</Route>
-
-								<ProtectedRoute exact path="/" component={Home} />
-								<ProtectedRoute
-									exact
-									path="/all-birthdays"
-									component={AllBirthdays}
-								/>
-
-								<ProtectedRoute exact path="/add-birthday" component={AddForm} />
-
-								<ProtectedRoute
-									exact
-									path="/edit-birthday/:id"
-									component={EditForm}
-								></ProtectedRoute>
-
-								<ProtectedRoute exact path="/search" component={Search} />
-								<ProtectedRoute
-									exact
-									path="/upcoming-birthdays"
-									component={UpcomingBirthdays}
-								/>
-
-								<Route path="*">
-									<PageNotFound />
-								</Route>
-							</Switch>
+							{/* we want to perform code splitting at each route */}
+							<Suspense fallback={<LoadingSpinner />}>
+								<Switch>
+									<Route path="/signup">
+										<Signup />
+									</Route>
+									<Route path="/login">
+										<Login />
+									</Route>
+									<Route path="/reset-password">
+										<ResetPassword />
+									</Route>
+									<ProtectedRoute exact path="/" component={Home} />
+									<ProtectedRoute exact path="/all-birthdays" component={AllBirthdays} />
+									<ProtectedRoute exact path="/add-birthday" component={AddForm} />
+									<ProtectedRoute
+										exact
+										path="/edit-birthday/:id"
+										component={EditForm}
+									></ProtectedRoute>
+									<ProtectedRoute exact path="/search" component={Search} />
+									<ProtectedRoute exact path="/upcoming-birthdays" component={UpcomingBirthdays} />
+									<Route path="*">
+										<PageNotFound />
+									</Route>
+								</Switch>
+							</Suspense>
 						</main>
 					</div>
 				</GlobalContextProvider>
